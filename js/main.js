39f4ded83dbc98542daf2b4b55564c553760e0f8
boluge <boluge@gmail.com>
2013-06-27 22:39:00 +0200

    new files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function () {
+	// jQuery(window).bind('orientationchange', function(){
+	// 	if (window.orientation % 180 == 0){
+	// 		jQuery(document.body).css("-webkit-transform-origin", "").css("-webkit-transform", "");               
+	// 	} 
+	// 	else {                   
+	// 		if ( window.orientation > 0) { //clockwise
+	// 			jQuery(document.body).css("-webkit-transform-origin", "200px 190px")
+	// 				.css("-webkit-transform",  "rotate(-90deg)");  
+	// 		}
+	// 	   else {
+	// 			jQuery(document.body).css("-webkit-transform-origin", "280px 190px")
+	// 				.css("-webkit-transform",  "rotate(90deg)"); 
+	// 		}
+	// 	}
+	// })
+	// .trigger('orientationchange'); 
+});
